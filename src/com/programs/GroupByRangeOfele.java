0d19e01a9e35b074c0507cf9f771fc5c0240c373
Harish Bandamravuri <harish_bandamravuri@epam.com>
2022-09-17 16:15:25 +0530

    add

---
+++ src/com/programs/GroupByRangeOfele.java
@@ -0,0 +1,18 @@
+package com.programs;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class GroupByRangeOfele {
+
+	public static void main(String[] args) {
+
+		Map<Integer, List<Integer>> map = Stream.of(2, 34, 54, 23, 33, 20, 59, 11, 19, 37)
+        .collect(Collectors.groupingBy (i -> i/10 * 10 ) );        
+        System.out.println(map);
+        System.out.println(34/10*10);
+	}
+
+}
